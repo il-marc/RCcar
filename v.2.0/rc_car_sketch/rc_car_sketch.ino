2c16e1032fee2ce9989089642c1b98e01d81e9c8
il-marc <il-marc@ya.ru>
2014-01-20 23:11:25 +0200

    update

--- v.2.0/rc_car_sketch/rc_car_sketch.ino
+++ v.2.0/rc_car_sketch/rc_car_sketch.ino
@@ -1,71 +1,116 @@
-#include <Servo.h>
-
+/*********************************************************
+* RCcar
+*
+* This sketch, for the Arduino, driving cars, depending on 
+* commands received by bluetooth module HC-04.
+*
+* by Ilia Marchenko
+* 
+* https://github.com/il-marc/RCcar
+**********************************************************/
+// commands definition
+#define FORWARD_COMMAND  'w'
+#define BACKWARD_COMMAND 's'
+#define STOP_COMMAND     ' '
+#define LEFT_COMMAND     'a'
+#define RIGHT_COMMAND    'd'
+#define CENTER_COMMAND   'q'
+#define VOLTAGE_COMMAND  'V'
+#define SPEED_COMMAND    'S'
+// PINSs definition
 #define VOLTAGE_PIN A0
 #define SERVO_PIN 11
-#define ENABLE 10
-#define INPUT1 9
-#define INPUT2 8
+#define DRIVER_ENABLE 10
+#define DRIVER_INPUT_1 9
+#define DRIVER_INPUT_2 8
+// servo settings
+#define SERVO_LEFT 544
+#define SERVO_MID 1500
+#define SERVO_RIGHT 2400
+#include <Servo.h>
 
 Servo servo;
-char incomingByte = 0;
-int voltageValue;
-char voltageValueByte;
-
+// the setup routine runs once when you press reset
 void setup() {
+  // servo setting up
   servo.attach(SERVO_PIN);
-  pinMode(ENABLE, OUTPUT);
-  pinMode(INPUT1, OUTPUT);
-  pinMode(INPUT2, OUTPUT);
+  center();
+  // L293DD driver setting up
+  pinMode(DRIVER_ENABLE, OUTPUT);
+  pinMode(DRIVER_INPUT_1, OUTPUT);
+  pinMode(DRIVER_INPUT_2, OUTPUT);
+  brake();
+  // bluetooth HC-04 setting up
   Serial.begin(9600);
 }
+// the loop routine runs over and over again forever
 void loop() {
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    switch (incomingByte) {
-      case 'w': forward();
+  if (Serial.available()) {
+    switch (Serial.read()) {
+      case FORWARD_COMMAND: forward();
         break;
-      case 's': backward();
+      case BACKWARD_COMMAND: backward();
         break;
-      case ' ': brake();
+      case STOP_COMMAND: brake();
         break;
-      case 'a': left();
+      case LEFT_COMMAND: left();
         break;
-      case 'd': right();
+      case RIGHT_COMMAND: right();
         break;
-      case 'q': center();
+      case CENTER_COMMAND: center();
         break;
-      case 'v': 
-        voltageValue = analogRead(voltageValue);
-        voltageValueByte = map(voltageValue, 0, 1023, 0, 255);
-        Serial.print(voltageValueByte);
+      case VOLTAGE_COMMAND: measureVoltage();        
+        break;
+      case SPEED_COMMAND: measureSpeed();        
         break;
     }                     
   } 
 }
+
 void forward() {
-  digitalWrite(ENABLE,LOW);   
-  digitalWrite(INPUT1,HIGH);
-  digitalWrite(INPUT2,LOW);
-  digitalWrite(ENABLE,HIGH);  
+  digitalWrite(DRIVER_ENABLE,LOW);   
+  digitalWrite(DRIVER_INPUT_1,HIGH);
+  digitalWrite(DRIVER_INPUT_2,LOW);
+  digitalWrite(DRIVER_ENABLE,HIGH);  
 }
+
 void backward() {
-  digitalWrite(ENABLE,LOW);   
-  digitalWrite(INPUT1,LOW);
-  digitalWrite(INPUT2,HIGH);
-  digitalWrite(ENABLE,HIGH); 
+  digitalWrite(DRIVER_ENABLE,LOW);   
+  digitalWrite(DRIVER_INPUT_1,LOW);
+  digitalWrite(DRIVER_INPUT_2,HIGH);
+  digitalWrite(DRIVER_ENABLE,HIGH); 
 }
+
 void brake() {
-  digitalWrite(ENABLE,LOW);
-  digitalWrite(INPUT1,HIGH);
-  digitalWrite(INPUT2,HIGH);
-  digitalWrite(ENABLE,HIGH); 
+  digitalWrite(DRIVER_ENABLE,LOW);
+  digitalWrite(DRIVER_INPUT_1,HIGH);
+  digitalWrite(DRIVER_INPUT_2,HIGH);
+  digitalWrite(DRIVER_ENABLE,HIGH); 
 }
+
 void left() {
-  servo.write(75);
+  servo.writeMicroseconds(SERVO_LEFT);
 }
+
 void right() {
-  servo.write(105); 
+  servo.writeMicroseconds(SERVO_RIGHT);
 }
+
 void center() {  
-  servo.write(90);
+  servo.writeMicroseconds(SERVO_MID);
+}
+
+void measureVoltage() {
+  int voltageValue;
+  char voltageValueByte;
+  voltageValue = analogRead(voltageValue);
+  voltageValueByte = map(voltageValue, 0, 1023, 0, 255);
+  Serial.write(VOLTAGE_COMMAND);
+  Serial.write(voltageValueByte);
+  Serial.write('\0');
+}
+void measureSpeed() {
+  Serial.print(SPEED_COMMAND);
+  Serial.write('\0');
+  Serial.write('\0');
 }
